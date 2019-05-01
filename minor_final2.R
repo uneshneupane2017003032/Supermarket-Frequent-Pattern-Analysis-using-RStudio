c33693a6b1fee5d331441208227cb421186b934b
uneshneupane2017003032 <47621435+uneshneupane2017003032@users.noreply.github.com>
2019-05-01 11:05:49 +0530

    Add files via upload

--- minor_final2.R
+++ minor_final2.R
@@ -15,31 +15,24 @@ rules <- apriori(Groceries, parameter = list(supp = 0.001, conf = 0.8))
 options(digits=2)
 inspect(rules[1:5])
 
-# Shows a brief summary of the data
 summary(rules)
-
-# Sorting the data based on confidence
+ 
 rules<-sort(rules, by="confidence", decreasing=TRUE)
 
-# Applying Apriori based on user-defined support and confidence
 rules <- apriori(Groceries, parameter = list(supp = 0.001, conf = 0.8,maxlen=3))
 
-# Applying Apriori to establish relation with "Whole Milk" in Right Hand Side
+
 rules<-apriori(data=Groceries, parameter=list(supp=0.001,conf = 0.08), 
-               appearance = list(default="lhs",rhs="whole milk"),
+               appearance = list(default="lhs",rhs="sugar"),
                control = list(verbose=F))
 rules<-sort(rules, decreasing=TRUE,by="confidence")
 inspect(rules[1:5])
 
-# Applying Apriori to establish relation with "Whole Milk" in Left Hand Side
 rules<-apriori(data=Groceries, parameter=list(supp=0.001,conf = 0.15,minlen=2), 
                appearance = list(default="rhs",lhs="whole milk"),
                control = list(verbose=F))
 rules<-sort(rules, decreasing=TRUE,by="confidence")
-inspect(rules[1:5])
+inspect(rules[1:5]) 
 
-# Arules Visualizing Library
 library(arulesViz)
-
-# Plot visualization
 plot(rules,method="graph",interactive=TRUE,shading=NA)
